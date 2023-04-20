24cfc0ba340b1535cfc33e85b4e4c448c4c2072d
Amanuel2000 <amanuelf44@gmail.com>
2023-04-20 23:33:27 +0300

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
+"""determining pascal's triangle"""
+
 
 def pascal_triangle(m):
     """
-    returns a list of lists of integers representing the Pascal’s triangle of m
+    returns list of Pascal’s triangle of m
     """
     triangle = []
 
     # return (trianlgle if m <= 0)
     if m <= 0:
         return triangle
-    for j in range(m):
+    for h in range(m):
         temp_list = []
 
-        for k in range(j+1):
-            if k == 0 or k == i:
+        for i in range(h+1):
+            if i == 0 or i == h:
                 temp_list.append(1)
             else:
-                temp_list.append(triangle[j-1][k-1] + triangle[j-1][k])
+                temp_list.append(triangle[h-1][i-1] + triangle[h-1][i])
         triangle.append(temp_list)
     # print(triangle)
-    return 
+    return triangle

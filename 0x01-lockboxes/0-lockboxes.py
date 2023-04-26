e23798fd3a1be9e644b31fd65020a1887de7eb5b
Amanuel2000 <amanuelf44@gmail.com>
2023-04-26 18:10:40 +0300

    fixed errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,14 +8,14 @@ def canUnlockAll(boxes):
     (indices) to other boxes can be unlocked given that the first
     box is unlocked.
     '''
-    n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
+    m = len(boxes)
+    visible_boxes = set([0])
+    notVisible_boxes = set(boxes[0]).difference(set([0]))
+    while len(notVisible_boxes) > 0:
+        boxIdx = notVisible_boxes.pop()
+        if not boxIdx or boxIdx >= m or boxIdx < 0:
             continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+        if boxIdx not in visible_boxes:
+            notVisible_boxes = notVisible_boxes.union(boxes[boxIdx])
+            visible_boxes.add(boxIdx)
+    return m == len(visible_boxes)

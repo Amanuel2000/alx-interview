3389c4d873e50439a94cd40a6b4cb28019ef3213
Amanuel2000 <amanuelf44@gmail.com>
2023-05-14 19:49:08 +0300

    added

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,14 +8,11 @@ import sys
 # store the count of all status codes in a dictionary
 list_of_status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                      '404': 0, '405': 0, '500': 0}
-
 all_sizes = 0
 count = 0  # keep count of the number lines counted
-
 try:
     for line in sys.stdin:
         line_list = line.split(" ")
-
         if len(line_list) > 4:
             status_code = line_list[-2]
             file_size = int(line_list[-1])
